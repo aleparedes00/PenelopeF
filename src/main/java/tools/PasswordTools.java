f57f2d901aa282f6107db60cf5a1ea7ef7e6e0bf
Alexandre Hyvernaud <alexandre.hyvernaud@mappy.com>
2018-03-01 09:57:46 +0100

    Improved password tools with regex

--- src/main/java/tools/PasswordTools.java
+++ src/main/java/tools/PasswordTools.java
@@ -5,32 +5,40 @@ import java.util.Collections;
 import java.util.List;
 
 import static java.lang.Math.random;
+import static tools.RegexTools.match;
 
 public class PasswordTools {
     /* Password alphabets */
-    public static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
-    public static final String ALPHA_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String NUM = "0123456789";
-    public static final String SPECIAL = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    private static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
+    private static final String ALPHA_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUM = "0123456789";
+    private static final String SPECIAL = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+    private static final String passwordRegex = "^(?=.*[" + ALPHA + "])(?=.*[" + ALPHA_CAPS + "])(?=.*[" + NUM + "])" +
+            "(?=.*[" + SPECIAL + "])[" + ALPHA + ALPHA_CAPS + NUM + SPECIAL + "]{8,}";
 
     public static String generatePassword() {
         Character[] pwdBuilder = new Character[8];
 
+        // At least one lowercase letter
+        int random_char = (int) (random() * ALPHA.length());
+        pwdBuilder[0] = ALPHA.charAt(random_char);
+
         // At least one capital letter
-        int random_char = (int) (random() * ALPHA_CAPS.length());
-        pwdBuilder[0] = ALPHA_CAPS.charAt(random_char);
+        random_char = (int) (random() * ALPHA_CAPS.length());
+        pwdBuilder[1] = ALPHA_CAPS.charAt(random_char);
 
         // At least one number
         random_char = (int) (random() * NUM.length());
-        pwdBuilder[1] = NUM.charAt(random_char);
+        pwdBuilder[2] = NUM.charAt(random_char);
 
         // At least one SPECIAL character
         random_char = (int) (random() * SPECIAL.length());
-        pwdBuilder[2] = SPECIAL.charAt(random_char);
+        pwdBuilder[3] = SPECIAL.charAt(random_char);
 
         // Fill the rest
         String alphabet = ALPHA + ALPHA_CAPS + NUM + SPECIAL;
-        for (int i = 3; i < 8; i++) {
+        for (int i = 4; i < 8; i++) {
             random_char = (int) (random() * alphabet.length());
             pwdBuilder[i] = alphabet.charAt(random_char);
         }
@@ -47,19 +55,6 @@ public class PasswordTools {
     }
 
     public static boolean hasRightFormat(String pwd) {
-        boolean hasCap = false;
-        boolean hasNum = false;
-        boolean hasSpecial = false;
-
-        for (int i = 0; i < pwd.length(); i++) {
-            if (ALPHA_CAPS.contains("" + pwd.charAt(i)))
-                hasCap = true;
-            if (NUM.contains("" + pwd.charAt(i)))
-                hasNum = true;
-            if (SPECIAL.contains("" + pwd.charAt(i)))
-                hasSpecial = true;
-        }
-
-        return (hasCap && hasNum && hasSpecial);
+        return match(pwd, passwordRegex);
     }
 }
